45657dcd8fdd997dbfaf0efdd137bcdb9a6bedcf
kabusch-sec <81918182+kabusch940@users.noreply.github.com>
2025-11-29 13:49:43 +0100

    Refactor inventory server addition logic for clarity and efficiency

--- CloudRiskAuditor.ps1
+++ CloudRiskAuditor.ps1
@@ -1,53 +1,37 @@
-### Aktuelle Windows Version (keep up to date)
+## Inveotry System ArrayLists
 
-[string]$currentWindowsVersion = "25H2"
+$dbServers              = [System.Collections.ArrayList]@("databaseServer1", "databaseServer2", "databaseServer3")
+$fileServers            = [System.Collections.ArrayList]@("fileserver1", "fileserver2", "fileserver3")
+$DCs                    = [System.Collections.ArrayList]@("DC1")
+$unclassifiedServers    = [System.Collections.ArrayList]@()
 
-## Inventory System erstellen mit Arrays
 
-$dbServers   = [System.Collections.ArrayList]@("databaseServer1", "databaseServer2", "databaseServer3")
-$fileServers = [System.Collections.ArrayList]@("fileserver1", "fileserver2", "fileserver3")
-$DCs         = [System.Collections.ArrayList]@("DC1")
+# Input New Servers
 
 [string]$newServerInput = Read-Host "Enter Servers to be added to the inventory (Comma seperated), or press Enter to skip"
 
-## Remove Whitespace from Input
 
-if ($newServerInput.Trim()){
-    $newServer = $newServerInput -split ',' | ForEach-Object { $_.Trim()} | Where-Object { $_ -ne '' }
-    foreach($newServer in $newServers) {
-        $serverLower = $newServers.ToLower
-        if($serverLower -like "*file*"){
-            $fileServers.Add($serverLower)
-            Write-Host "Added" $serverLower "to FileServers."
-        } elseif($serverLower -like "*dc*"){
-            $DCs.Add($serverLower)
-            Write-Host "Added" $serverLower "to DCServers."
-        } elseif ($serverLower -like "*database*"){
-            $dbServers.Add($serverLower)
-            Write-Host "Added" $serverLower "to DBServers."
-        } else {
-            Write-Host "Unclassified Name:" $serverLower
-        }
+# Add New Server to Inventory
+
+if($newServerInput){
+    $newServers = $newServerInput -split ',' | ForEach-Object { $_.Trim().ToLower() } | Where-Object { $_ -ne '' }
+} foreach($newServer in $newServers){
+    if ($newServer -like "*file*") {
+        $fileServers += $newServer | Out-Null
+        Write-Host $newServer "added to Fileservers"
+    } elseif($newServer -like "*dc*") {
+        $DCs += $newServer | Out-Null
+        Write-Host $newServer "added to DCs"
+    } elseif ($newServer -like "*db*") {
+        $dbServers += $newServer | Out-Null
+        Write-Host $newServer "added to DBServers"
+    } else {
+        $unclassifiedServers += $newServer | Out-Null
+        Write-Host $newServer "is unclassified."
     }
 }
 
-$DCs
 
-
-
-
-
-
-
-## Inventory Add Abfrage (willst du einen Server hinzufügen?)
-
-## Per PSRemote auf die invetarisierten Server zugreifen
-
-## Installierte Programme auslesen und Programmversionen und in Array speichern
-
-## Installierte Windows Version auslesen und mit momentaner Windows Version vergleichen
-
-## REST API CVE aufrufen (Funktion)
 
 
 
